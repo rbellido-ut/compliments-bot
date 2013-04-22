cfb71d027c539dc8fdf6472913c086d4e46a573a
Ronald <rbellido54@gmail.com>
2013-04-21 20:10:05 -0700

    can send out a complete compliment tweet

--- compliment-bot.py
+++ compliment-bot.py
@@ -1,6 +1,15 @@
 import random
+import time
+import sys
+import signal
 
 from twitter import Twitter, OAuth, read_token_file
+
+# Let's handle keyboard interrupts gracefully
+def keyb_interrupt_handler(signal, frame):
+	print '\nBye!'
+	sys.exit(0)
+signal.signal(signal.SIGINT, keyb_interrupt_handler)
 
 # get the list of compliments
 f = open('compliments.txt', 'r')
@@ -28,11 +37,25 @@ twitter = Twitter(auth=OAuth(oauth_token,
 #print mentioners[0]['entities']['user_mentions'][0]['screen_name']
 
 # start loop
-# check if the id of the first mention is different from the current
+# check if the id of the first mention is different from the previous id
 # if different:
 #	tweet a random compliment
-# sleep for 5 mins
+#	update previd
+# sleep for 1 min
 
-#while True:
 mentioners = twitter.statuses.mentions_timeline()
-print mentioners[0]['id']
+previd = mentioners[0]['id']
+
+while True:
+	mentioners = twitter.statuses.mentions_timeline()
+	newid = mentioners[0]['id']
+
+	if (previd != newid):
+		replyto = mentioners[0]['entities']['user_mentions'][0]['screen_name']
+		message = '@%s %s' % replyto, random.choice(compliments)
+		#twitter.statuses.update(status=message)
+		print message
+		previd = mentioners[0]['id']
+
+	# sleep for 1 minute before continuing again
+	time.sleep(60)
